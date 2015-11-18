71a9d97c13516d4524f5aee94d77819581f7067f
levi006 <le.vi006@gmail.com>
2015-11-18 15:22:14 -0800

    Added BinaryGate function.

--- python-review/logic-gates.py
+++ python-review/logic-gates.py
@@ -9,3 +9,16 @@ class LogicGate:
 		def getOutput(self):
 			self.outpout = self.performGateLogic()
 			return self.output 
+
+class BinaryGate(LogicGate):
+		def __init__(self,n):
+			super().__init__(n)
+
+			self.pinA = None
+			self.pinB = None
+
+		def getPinA(self):
+			return int(input("Enter Pin A input for gate" + self.getLabel() + "-->"))
+
+		def getPinB(self):
+			return int(input("Enter Pin B input for gate" + self.getLabel() + "-->"))
