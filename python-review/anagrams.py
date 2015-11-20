76a6b6ba19080049abbbad235b2380f4ef076bb0
levi006 <le.vi006@gmail.com>
2015-11-20 14:02:54 -0800

    Added a third solution: count and compare.

--- python-review/anagrams.py
+++ python-review/anagrams.py
@@ -67,5 +67,18 @@ def anagramSolution3(s1,s2):
 	c2 = [0]*26
 
 	for i in range(len(s1)):
-		pos = ord()
+		pos = ord(s1[i]) - ord('a')
+		c1[pos] = c1[pos] + 1 
+
+	j = 0 
+	stillOK = True
+	while j<26 and stillOK: 
+		if c1[j] == c2[j]:
+			j = j + 1
+		else:
+			stillOK = False
+
+	return stillOK
+
+	 
 
