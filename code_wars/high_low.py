2f9eab890f248adea354c1392681bc0edc413093
Vi Le <le.vi006@gmail.com>
2016-12-13 01:54:33 -0800

    Added second solution to high_low.py using max()

--- code_wars/high_low.py
+++ code_wars/high_low.py
@@ -12,6 +12,9 @@ Example:
 >>> high_and_low("1 9 3 4 -5")
 '9 -5'
 
+>>> high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
+'542 -214'
+
 Notes:
 
 All numbers are valid Int32, no need to validate them.
@@ -19,27 +22,35 @@ There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 
 """
+# def high_and_low(numbers):
+#     number_list = numbers.strip().split()
+#     integer_list = map(int, number_list)
+#     # print integer_list
+#     # print type(integer_list)
+    
+#     high = integer_list[0]
+#     low = integer_list[0]
+    
+#     for num in range(len(integer_list)):
+#         if integer_list[num] < low:
+#             low = integer_list[num]
+#         if integer_list[num] > high:
+#             high = integer_list[num]
+
+#         high_low = str(high), str(low)
+#         numbers = " ".join(high_low)
+#         # print repr(numbers)
+#         # print type(numbers)    
+#     return numbers
+
 def high_and_low(numbers):
-    number_list = numbers.strip().split()
-    integer_list = map(int, number_list)
-    print integer_list
-    print type(integer_list)
+    string_list = numbers.strip().split()
     
-    high = integer_list[0]
-    low = integer_list[0]
-    
-    for num in range(len(integer_list)):
-        if integer_list[num] < low:
-            low = integer_list[num]
-        if integer_list[num] > high:
-            high = integer_list[num]
+    int_list = []
 
-        high_low = str(high), str(low)
-        numbers = " ".join(high_low)
-        print repr(numbers)
-        print type(numbers)    
-    return numbers
-
+    for i in range(len(string_list)):
+        int_list.append(int(string_list[i]))
+    return str(max(int_list)) + " " + str(min(int_list))
 
 if __name__ == '__main__':
     import doctest
