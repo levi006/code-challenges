0bca7db6bcd01f93c3adf7ac1e1d76cda589a4ce
Vi Le <le.vi006@gmail.com>
2016-12-17 17:40:43 -0800

    Accessed longest word in dictionary via 1) iteritems() and 2) keys(), values() and index()

--- code_wars/longest_consec_str.py
+++ code_wars/longest_consec_str.py
@@ -10,33 +10,42 @@ n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
 """
 
 def longest_consec(strarr,k):
-	string_dict = {}
+    string_dict = {}
 
-	# .get() returns value for key
-	
-	# key = len(strarr[0])
-	# value = strarr[0]
-	# string_dict[key] = value
+    # .get() returns value for key
+    
+    # key = len(strarr[0])
+    # value = strarr[0]
+    # string_dict[key] = value
 
-	#create dictionary {'word': word_len}
-	for i in range(len(strarr)):
-		string_dict[strarr[i]] = len(strarr[i])
-	# print "dictionary is " + str((string_dict))
-	# print str(string_dict)
-	
-	longest_len = max(string_dict.values())
-	print "longest length is " + str(longest_len)
+    #create dictionary {'word': word_len}
+    for i in range(len(strarr)):
+        string_dict[strarr[i]] = len(strarr[i])
+    # print "dictionary is " + str((string_dict))
+    # print "list of items in string_dict: " + str(string_dict.items())
+    print string_dict
 
-	# print string_dict.keys() 
+    longest_len = max(string_dict.values())
+    print "longest length is: " + str(longest_len)
 
-	#list of word lengths, sorted min to max
-	word_count = sorted(string_dict.values())
-	print string_dict.items()
-	print max(string_dict[i][1].items())
+    word_count = sorted(string_dict.values())
+    print "sorted list of word counts: " + str(word_count)
+    print "sorted list by alphabetized keys: " + str(sorted(string_dict.items()))
+    print "sorted list of alphabetized keys: " + str(sorted(string_dict.keys()))
+    max_value = max(map(int, string_dict.values()))
+    print "max_length: " + str(max_value)
 
-	# for i in range(k):
-	# 	concat_str = ''
-	# 	''.join(word_count[::-1])
-	# return concat_str
-	
+
+    for word, length in string_dict.iteritems():
+        if max_value == length:
+            print word
+            print list(string_dict.keys())[list(string_dict.values()).index(length)] # Prints george
+
+   
+
+
+
+    # for i in range(k):
+ 
+    
 longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"],2)
