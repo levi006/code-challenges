005d7e30368d73b3c72a92d09887d527cc49095c
Vi Le <le.vi006@gmail.com>
2017-05-16 00:15:39 -0700

    working doctests

--- code_wars/time_correct.py
+++ code_wars/time_correct.py
@@ -17,16 +17,16 @@ If the time-string-format is invalid, return null. (empty string for C++)
 ' '
 
 >>> time_correct("09:10:01")
-"09:10:01"
+'09:10:01'
 
 >>> time_correct("11:70:10")
-"12:10:10"
+'12:10:10'
 
 >>> time_correct("19:99:99")
-"20:40:39"
+'20:40:39'
 
 >>> time_correct("24:01:01")
-"00:01:01"
+'00:01:01'
 
 """
 
@@ -36,6 +36,35 @@ def time_correct(t):
     
     if t == None:
         return None
+
+    h, m, s = t.strip().split(":")
+    
+    hrs, mins, secs = int(h), int(m), int(s)
+
+    if secs >= 60:
+    	secs_remainder = secs - 60
+    	mins = mins + 1
+    	secs =secs_remainder
+    elif secs < 10:
+    	secs = "0" + str(secs)
+
+    if mins >= 60:
+    	mins_remainder = mins - 60
+    	hrs = hrs + 1
+    	mins = mins_remainder
+    elif mins < 10:
+    	mins = "0" + str(mins)
+
+    if hrs == 24:
+    	hrs = "00"
+    elif hrs < 10:
+    	hrs = "0" + str(hrs)
+
+
+    time = str(hrs) + ":" + str(mins) + ":" + str(secs) 
+
+    return time
+
 
 
 if __name__ == '__main__':
