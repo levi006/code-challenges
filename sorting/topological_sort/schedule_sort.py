c7cbc12b6cf22353961a7778712c5be038bbe7dd
Vi Le <le.vi006@gmail.com>
2017-05-27 23:09:25 -0700

    added error handling

--- sorting/topological_sort/schedule_sort.py
+++ sorting/topological_sort/schedule_sort.py
@@ -1,25 +1,49 @@
+#!/usr/bin/env python
+
+""" ... """
+
 import sys, json
-from pprint import pprint
 
-def scheduler(course_catalog):
-    with open(sys.argv[1]) as schedule_data:    
-        course_catalog = json.load(schedule_data)
+# courses_reqs
 
-    sorted_schedule = []
+def add_ready_courses(course_catalog, sorted_schedule):
+    """ ... """
 
-    for course in course_catalog:
+    for idx, course in enumerate(course_catalog):
         prereqs = course['prerequisites']
         name = course['name']
 
-        if prereqs == []:
+        # if prereqs == [] or set(prereqs).issubset(sorted_schedule):
+        if set(prereqs).issubset(sorted_schedule):
             sorted_schedule.append(name)
+            # NOW CAN DELETE THIS FROM CATALOG
+            del course_catalog[idx]
+            return
 
-        elif set(prereqs).issubset(sorted_schedule):
-            sorted_schedule.append(name)    
+    # Oh no, went through entire catalog and nothing could
+    # be resolved
+
+    raise RuntimeError("Error: unresolvable loop in requirements")
+
+def scheduler(filename):
+    """ ... """
+
+    with open(filename) as schedule_data:    
+        course_catalog = json.load(schedule_data)
+
+    # List of courses as can  be performed
+    sorted_schedule = []
+
+    #
+
+    while course_catalog != []:
+        add_ready_courses(course_catalog, sorted_schedule)
+        # print "re-looping"
 
     for course in sorted_schedule:
         print(course)
 
     return sorted_schedule
 
-(scheduler(sys.argv[1]))
+json_filename = sys.argv[1] if len(sys.argv) > 1 else "../../../clever/math.json"
+scheduler(json_filename)
