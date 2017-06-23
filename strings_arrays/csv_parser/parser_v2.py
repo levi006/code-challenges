b6168470aa5317dc86d06f681db8d6992fbfed1a
Vi Le <le.vi006@gmail.com>
2017-06-23 09:16:00 -0700

    refactored processing code into separate function, added tests

--- strings_arrays/csv_parser/parser_v2.py
+++ strings_arrays/csv_parser/parser_v2.py
@@ -1,103 +1,192 @@
-
-import csv, re, datetime
+import csv
+import re
+import datetime
 
 def fix_acct_num(acct_num):
+    """Fix account number: ensure it padded with zeroes to length of 6.
+    
+        >>> fix_acct_num("123456")
+        '123456'
 
-	if len(acct_num) <= 6:
-		acct_num = "%06d" % int(acct_num)
+        >>> fix_acct_num("123")
+        '000123'
 
-	return acct_num
+        >>> fix_acct_num("ABC123")
+        Traceback (most recent call last):
+            ...
+        ValueError: Invalid account num: ABC123
+    """
+
+
+    if not acct_num.isdigit():
+        raise ValueError("Invalid account num: " + acct_num)
+
+    if len(acct_num) < 6:
+        acct_num = "%06d" % int(acct_num)
+
+    return acct_num
 
 def fix_date(date):
+    """Fix date: reformat 'Dec 1 2017' => '20171201'
+    
+        >>> fix_date('Dec 1 2017')
+        '20171201'
 
-	stripped_date = date.strip()
-	valid_date = datetime.datetime.strptime(stripped_date, "%b %d %Y").strftime("%Y%m%d")
+        >>> fix_date('Dec 32 2017')
+        Traceback (most recent call last):
+            ...
+        ValueError: time data 'Dec 32 2017' does not match format '%b %d %Y'
+    """
 
-	return valid_date
+    date = datetime.datetime.strptime(date, "%b %d %Y").strftime("%Y%m%d")
+
+    return date
 
 def fix_address(address):
+    """No cleanup needed for address.
+    
+        >>> fix_address('123 Main Street')
+        '123 Main Street'
+    """
 
-	address = address.strip()
+    address = address.strip()
 
-	return address
+    return address
 
 def fix_zipcode(zipcode):
-	"""Zipcodes less than 5 digits long are padded with leading zeros. """
-	
-	zipcode = zipcode.strip()
+    """Zipcodes less than 5 digits long are padded with leading zeros. 
 
-	if len(zipcode) <= 5:
-		zipcode = "%05d" % int(zipcode)
+        >>> fix_zipcode('12345')
+        '12345'
 
-	return zipcode
+        >>> fix_zipcode('123')
+        '00123'
+
+        >>> fix_zipcode('ABCDE')
+        Traceback (most recent call last):
+            ...
+        ValueError: Invalid zipcode: ABCDE
+    """
+
+    if not zipcode.isdigit():
+        raise ValueError("Invalid zipcode: " + zipcode)
+
+    if len(zipcode) < 5:
+        zipcode = "%05d" % int(zipcode)
+
+    return zipcode
 
 def fix_consumption(consumption):
-	"""Values containing commas are treated as string literals."""
-	
-	consumption = consumption.strip()
-	if consumption.isdigit():
-		consumption = int(consumption)
-	else:
-		
-		sections = re.split('[^0-9]+', consumption)
-		
-		num = " "
-		
-		for section in sections:
-			if section.isdigit():
-				num += section
+    """Validate consumption.
 
-		consumption = int(num)
+    Return an integer, removing commas if present:
 
-	return consumption
+        >>> fix_consumption('123')
+        123
 
-# transforms fixed width input file into csv
-with open('example_input.txt', 'r') as txt_file, open('transformed.csv', 'w') as csv_file:
+        >>> fix_consumption('1,213')
+        1213
 
-	writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
-	writer.writerow(('Account Number', 'Read Date', 'Address', 'Zip Code', 'Consumption'))
-	
-	possible_dupes = dict()
+    Interpret a (number) as negative:
 
-	for line in txt_file:
-		
-		# defining columns in fixed width input file
-		acct_num = line[0:6]
-		date = line[6:17]
-		address = line[21:44]
-		zipcode = line[44:49]
-		consumption = line[54:]
-		
-		# normalizing functions for each column 
-		acct_num = fix_acct_num(acct_num)
-		read_date = fix_date(date)
-		address = fix_address(address)
-		zipcode = fix_zipcode(zipcode)
-		consumption = fix_consumption(consumption)
+        >>> fix_consumption('(123)')
+        -123
 
-		new_row = [acct_num, read_date, address, zipcode, consumption]
-		writer.writerow(new_row)
+        >>> fix_consumption('(1,213)')
+        -1213
 
-		# records with the same account number and read date are logged as possible duplicates
-		key = (acct_num, read_date)
+    Return error if invalid:
 
-		if key not in possible_dupes:
-			possible_dupes[key] = [new_row]
-		
-		elif key in possible_dupes:
-			possible_dupes[key].append(new_row)
+        >>> fix_consumption('ABC')
+        Traceback (most recent call last):
+            ...
+        ValueError: Invalid consumption: ABC
 
-# possible duplicates are written out to a csv file
-with open('duplicates.csv', 'w') as dupes:
+        >>> fix_consumption('(123')  # no closing paren
+        Traceback (most recent call last):
+            ...
+        ValueError: Invalid consumption: (123
+    """
+    
+    if consumption[0] == "(" and consumption[-1] == ")":
+        consumption = consumption[1:-1]
+        sign = -1
 
-	dupe_writer = csv.writer(dupes, delimiter=',', lineterminator='\n')
-	dupe_writer.writerow(('Account Number', 'Read Date', 'Address', 'Zip Code', 'Consumption'))
-	for key in possible_dupes:
-		if len(possible_dupes[key]) > 1:
-			dupe_writer.writerows(possible_dupes[key])
-			
+    else:
+        sign = +1
 
+    consumption = consumption.replace(",", "")
+
+    if not consumption.isdigit():
+        raise ValueError("Invalid consumption: " + consumption)
+
+    return int(consumption) * sign
 
 		
+def extract_line_to_row(line):
+    """Process a line of input in a row of data.
+
+        >>> extract_line_to_row(
+        ...     "81769 Feb 25 2017    25 California Way      94111     105  ")
+        ['081769', '20170225', '25 California Way', '94111', 105]
+
+    """
+    # defining columns in fixed width input file
+    acct_num = line[0:6].strip()
+    date = line[6:17].strip()
+    address = line[21:44].strip()
+    zipcode = line[44:49].strip()
+    consumption = line[54:].strip()
+
+    # normalizing functions for each column 
+    acct_num = fix_acct_num(acct_num)
+    read_date = fix_date(date)
+    address = fix_address(address)
+    zipcode = fix_zipcode(zipcode)
+    consumption = fix_consumption(consumption)
+
+    new_row = [acct_num, read_date, address, zipcode, consumption]
+
+    return new_row
+
+def process_file(in_file, out_file, dupe_file):
+    """Process CSV file, writing CSV output and duplicate-record output."""    
+    
+    headers = ['Account', 'Read Date', 'Address', 'Zip Code', 'Consumption']
+
+    with open(in_file, 'r') as txt_file, \
+         open(out_file, 'w') as csv_file, \
+         open(dupe_file, 'w') as dupe_file:
+
+        writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
+        writer.writerow(headers)
+
+        dupe_writer = csv.writer(dupe_file, delimiter=',', lineterminator='\n')
+        dupe_writer.writerow(headers)
+        
+        possible_dupes = set()
+
+        for line in txt_file:
+
+            data = extract_line_to_row(line)
+    		
+            # records with the same account number and read date are logged as possible duplicates
+            key = (data[0], data[1])
+
+            if key not in possible_dupes:
+            	possible_dupes.add(key)
+                writer.writerow(data)
+
+            # possible duplicates are written out to a csv file
+            else:
+                dupe_writer.writerow(data)
+
+
+process_file("example_input.txt", "transformed.csv", "dupes.csv")		
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. !\n"
 
 		
