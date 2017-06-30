b5b56375b4b30ead409c0b53a69d325e3adacd6e
Vi Le <le.vi006@gmail.com>
2017-06-30 16:05:17 -0700

    cleaned up stddev

--- strings_arrays/csv_parser/stddev.py
+++ strings_arrays/csv_parser/stddev.py
@@ -13,11 +13,11 @@ def stddev(number_list):
  
     average = sum(number_list) / float(len(number_list))
      
-    stdev = 0
+    variance = 0
     for value in number_list:
-        stdev += ((average - value)**2) / float(len(number_list))
-     
-    return math.sqrt(stdev)
+        variance += ((average - value)**2) / float(len(number_list))
+    stddev = math.sqrt(variance)
+    return stddev
 
 print(standard_deviation([1,2,3,4,5]))
 print(stddev([1,2,3,4,5]))
